bc88e63f6776e789e2e736fed1c5ac69c5ee316e
Thomas Farvour <tom@farvour.com>
2020-11-11 21:19:24 -0800

    Add an entrypoint script.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,3 +2,18 @@
 set -xe
 
 echo "Starting server..."
+
+: ${MAP_NAME:="TheIsland"}
+: ${SESSION_NAME:=""}
+: ${SERVER_PASSWORD:=""}
+: ${SERVER_ADMIN_PASSWORD:=""}
+
+[ -z "${SESSION_NAME}" ] && echo "Must set \$SESSION_NAME!" && exit 1
+
+# This comes from the Dockerfile/docker ENV.
+cd ${SERVER_INSTALL_DIR}
+
+./ShooterGameServer \
+    ${MAP_NAME}?listen?SessionName=${SESSION_NAME}?ServerPassword=${SERVER_PASSWORD}?ServerAdminPassword=${SERVER_ADMIN_PASSWORD} \
+    -server \
+    -log
